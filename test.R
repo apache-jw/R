75b4caaa7a16f7e66c592180dd0d1b183541dae6
jjw-devMsis <142926472+jjw-devMsis@users.noreply.github.com>
2024-01-11 12:29:02 -0500

    update packages

--- test.R
+++ test.R
@@ -8,3 +8,15 @@ library(devtools)
 
 
 ### re tooling test
+
+library(tidyverse)
+
+tidyverse_conflicts()
+tidyverse_update()
+
+
+install.packages(c("dplyr", "haven", "jsonlite", "ragg", "readr", "reprex", "rlang",
+"stringr", "xml2"))
+
+
+library(datasets)
