8049df59501afc90bc9c67a92853ec3b25324ecc
jjw-devMsis <142926472+jjw-devMsis@users.noreply.github.com>
2024-01-11 15:05:43 -0500

    new files

---
+++ HandsOn-Basics.R
@@ -0,0 +1,37 @@
+# Some basic operations in R
+#---------------------------------------------------------------------------------#
+
+# P1: Variable initialization
+#   1. Create a vector with values 12, 13, 14, 15, 16	and	199.
+#   2. Calculate the average of these values using the mean() function
+
+
+
+# P2: Import a csv data file
+# Download the "iris.csv" file from Canvas. Import it as a dataframe called IrisData
+
+
+
+
+# P3: accessing values in a data frame
+# Find the value on row 15, column 4
+
+
+# Extract all values of variable "sepal_length", using the "$" operator
+
+
+
+# P4: Import a csv data file with special delimiter
+# Download the "coffee.csv" file from Canvas. Import it, and store it as CoffeeData
+# Note that the delimiter of this file is not comma, it is semicolon (;)
+# We need to specify the delimiter in read.csv() function with the "sep" parameter 
+# Get help from the "Help" tab on your right - lookup read.csv() function's help page
+
+
+
+
+# P5: Install packages
+# Install "arules" package on your computer, then load it
+# Write your code below to load the library:
+
+
---
+++ HandsOn-Data-Process.R
@@ -0,0 +1,48 @@
+# Data cleaning and visualization in R
+# We will use Coffee dataset to practice
+
+# load the dplyr package
+
+
+# Import Coffee data, use your code from last exercise:
+
+
+
+# P1: Arrange data
+# What is the ProductID of the most profitable coffee?
+# Write your code below:
+
+
+
+# P2: Subset data
+# Find the subset of data with Sales larger than 200
+# Write your code below:
+
+
+
+# P3: Group and Summarise data
+# Find out the total profit and average inventory level of each product (identified by a unique ProductID)
+
+
+
+# P4: Create a new variable, ProfitRatio, which equals Profit/Sales
+
+
+
+# P5: Make histogram
+# Plot the distribution of COGS
+# Write your code below:
+
+
+  
+# P6: Make boxplot
+# Plot the distribution of profit across different products
+# Write your code below:
+
+
+
+# P7: Make scatterplot
+# Plot the relationship between profit margin and inventory
+# Write your code below:
+
+
