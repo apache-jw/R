ed69e528d80c940c60397c8a0601e4d176e790e2
jjw-devMsis <142926472+jjw-devMsis@users.noreply.github.com>
2024-01-16 16:11:03 -0500

    new

---
+++ boston.R
@@ -0,0 +1,7 @@
+# Load the datasets package
+library(datasets)
+
+# Import the Boston housing dataset
+data(Boston)
+
+# Now you can use the Boston dataset for probability theory analysis
--- test.R
+++ test.R
@@ -34,3 +34,4 @@ install.packages("dplyr")
 
 
 
+
